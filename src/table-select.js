c24e0ba64a97ebbc616535916f969acbab96d3a8
Wendell Holden <wendellholden@hotmail.com>
2018-10-29 14:24:05 -0300

    now u can define the rows that will show checkboxes, using trclass when creating the table-select obj and a class with the same name.

--- src/table-select.js
+++ src/table-select.js
@@ -1,111 +1,63 @@
-function TableSelection(opts) {
-    var self = this;
-
-    self.tableid = opts.tableid;
-    self.actionbuttons = opts.actionbuttons;
-    self.onchange = opts.onchange;
-
-    // can ben 'hide' or 'disable'
-    self.actionbuttonsmode = opts.actionbuttonsmode || 'hide';
-
-    self.insert_checkboxes();
-    self.update_action_buttons();
-
-    $(self.maincheckbox).change(function() {
-        if (this.checked) {
-            self.select_all();
-        }
-        else {
-            self.unselect_all();
-        }
-    });
-
-    $(self.get_childs_query()).change(function() {
-        self.update_action_buttons();
-        if (opts.onchange) {
-            opts.onchange(self);
-        }
-    });
+function TableSelection(e) {
+    var t = this;
+    t.tableid = e.tableid,
+    t.actionbuttons = e.actionbuttons,
+    t.onchange = e.onchange,
+    t.trclass = e.trclass,
+    t.actionbuttonsmode = e.actionbuttonsmode || "hide",
+    t.insert_checkboxes(),
+    t.update_action_buttons(),
+    $(t.maincheckbox).change(function() {
+        this.checked ? t.select_all() : t.unselect_all()
+    }),
+    $(t.get_childs_query()).change(function() {
+        t.update_action_buttons(),
+        e.onchange && e.onchange(t)
+    })
 }
 TableSelection.prototype.insert_checkboxes = function() {
-    var self = this;
-
-    self.maincheckbox = $('<input type="checkbox" class="TS TS-select">');
-    $(self.tableid + ' thead tr').prepend($('<th>').append(self.maincheckbox));
-
-    $(self.tableid + ' tbody tr').each(function(index, item) {
-        $(item).prepend('<td><input type="checkbox" class="TS TS-item-select"></td>');
-    });
-};
-
+    var t = this;
+    var e = t.trclass;
+    t.maincheckbox = $('<input type="checkbox" class="TS TS-select">'),
+    $(t.tableid + " thead tr").prepend($("<th>").append(t.maincheckbox)),
+    $(t.tableid + " tbody tr" + ("" == e || e == undefined ? "" : "." + e)).each(function(e, t) {
+        $(t).prepend('<td><input type="checkbox" class="TS TS-item-select"></td>')
+    })
+},
 TableSelection.prototype.has_item_selected = function() {
-    var self = this;
-
-    var has_selected = false;
-    $(self.get_childs_query()).each(function(index, item) {
-        if ($(item).prop('checked')) {
-            has_selected = true;
-            // returns false breaks the jQuery.each function
-            return false;
-        }
-    });
-    return has_selected;
-};
-
+    var e = !1;
+    return $(this.get_childs_query()).each(function(t, n) {
+        if ($(n).prop("checked"))
+            return e = !0,
+            !1
+    }),
+    e
+},
 TableSelection.prototype.get_childs_query = function() {
-    var self = this;
-
-    return self.tableid + ' tbody tr td input:checkbox.TS-item-select';
-};
-
+    return this.tableid + " input:checkbox.TS-item-select"
+},
 TableSelection.prototype.update_action_buttons = function() {
-    var self = this;
-
-    if (self.actionbuttons) {
-        if (self.has_item_selected()) {
-            if (self.actionbuttonsmode === 'hide') {
-                $(self.actionbuttons).show();
-            }
-            else if (self.actionbuttonsmode === 'disable') {
-                $(self.actionbuttons).prop('disabled', false);
-            }
-        }
-        else {
-            if (self.actionbuttonsmode === 'hide') {
-                $(self.actionbuttons).hide();
-            }
-            else if (self.actionbuttonsmode === 'disable') {
-                $(self.actionbuttons).prop('disabled', true);
-            }
-        }
-    }
-};
-
+    var e = this;
+    e.actionbuttons && (e.has_item_selected() ? "hide" === e.actionbuttonsmode ? $(e.actionbuttons).show() : "disable" === e.actionbuttonsmode && $(e.actionbuttons).prop("disabled", !1) : "hide" === e.actionbuttonsmode ? $(e.actionbuttons).hide() : "disable" === e.actionbuttonsmode && $(e.actionbuttons).prop("disabled", !0))
+},
 TableSelection.prototype.get_selected_ids = function() {
-    var self = this;
-    var list_result = [];
-    $(self.get_childs_query() + ':checked').each(function(index, item){
-        list_result.push(item.parentElement.parentElement.id);
-    });
-    return list_result;
-};
-
+    var e = [];
+    return $(this.get_childs_query() + ":checked").each(function(t, n) {
+        e.push(n.parentElement.parentElement.id)
+    }),
+    e
+},
 TableSelection.prototype.select_all = function() {
-    var self = this;
-    $(self.get_childs_query() + ':visible').prop('checked', true);
-    self.maincheckbox.prop('checked', true);
-    self.update_action_buttons();
-    if (self.onchange) {
-        self.onchange(self);
-    }
-};
-
+    var e = this;
+    $(e.get_childs_query() + ":visible").prop("checked", !0),
+    e.maincheckbox.prop("checked", !0),
+    e.update_action_buttons(),
+    e.onchange && e.onchange(e)
+},
 TableSelection.prototype.unselect_all = function() {
-    var self = this;
-    $(self.get_childs_query() + ':visible').prop('checked', false);
-    self.maincheckbox.prop('checked', false);
-    self.update_action_buttons();
-    if (self.onchange) {
-        self.onchange(self);
-    }
+    var e = this;
+    $(e.get_childs_query() + ":visible").prop("checked", !1),
+    e.maincheckbox.prop("checked", !1),
+    e.update_action_buttons(),
+    e.onchange && e.onchange(e)
 };
